2cf392bd95a5a3285be16643141f397d8fac0a67
Eugen Soloviov <suenot@gmail.com>
2022-11-20 21:49:12 +0500

    @capacitor/geolocation boilerplate

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -2,6 +2,31 @@ import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, Io
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
+import { Geolocation } from '@capacitor/geolocation';
+import React, {useState, useCallback} from 'react';
+
+const GeolocationPage = () => {
+
+  const [loc, setLoc] = useState<any>(null);
+
+  const getCurrentPosition = useCallback(async () => {
+    const coordinates: any = await Geolocation.getCurrentPosition();
+    setLoc(coordinates);
+  }, []);
+
+  return (
+    <div>
+      <h1>Geolocation</h1>
+      <p>Your location is:</p>
+      <p>Latitude: {loc?.coords.latitude}</p>
+      <p>Longitude: {loc?.coords.longitude}</p>
+
+      <button onClick={getCurrentPosition}>
+        Get Current Location
+      </button>
+    </div>
+  );
+}
 
 const Page: React.FC = () => {
 
@@ -25,6 +50,7 @@ const Page: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name={name} />
+        <GeolocationPage />
       </IonContent>
     </IonPage>
   );
